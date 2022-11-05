9a93f0b4676b75905e091a4f25336cbd540bcca1
Irina <irakali86@gmail.com>
2022-11-05 17:24:41 +0300

    formating

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
 
-    public double calculate(double weight , double height) {
+    public double calculate(double weight, double height) {
 
         double imt = weight / (height * height);
         return imt;
